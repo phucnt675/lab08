ebc8e859e74167260f22985fe0cbc2c60d09fd7f
dev/phucdu <phucntpc08600@gmail.com>
2025-04-14 18:09:50 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CloundinaryService } from './service/common/cloundinary.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'lab08';
+  imageUrl : string = '';
+
+  constructor(
+    private cloudinary : CloundinaryService
+  ){
+
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.cloudinary.uploadImage(file).subscribe((res: any) => {
+        this.imageUrl = res.secure_url;
+        console.log('Uploaded:', this.imageUrl);
+      });
+    }
+  }
 }
